8409553ed5c1c4dc79eb37190677d5319039c933
Sharon's Mac <help@pencilevent.com>
2016-07-26 15:48:40 -0400

    Short project to show how tab bars work.

---
+++ tab-test/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  tab-test
+//
+//  Created by Sharon's Mac on 7/26/16.
+//  Copyright © 2016 Sharon McDermott. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
